31cd11f0ee6e8bc9a67cde14533a9e2781b7ee47
Greg Van Buren <g.vanburen@gmail.com>
2016-08-24 17:55:11 -0700

    link change

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from slugify import slugify
 
 
 post_paths = ('post_1.md', 'post_2.md')
+index_links = ['<ul>']
 links = ['<ul>']
 for post_path in post_paths:
 	md = Markdown(extensions=['markdown.extensions.meta'])
@@ -10,8 +11,11 @@ for post_path in post_paths:
 		html = md.convert(f.read())
 		title = md.Meta['title'][0]
 		slug = slugify(title)
-		links.append('<li><a href=pages/{slug}.html>{title}</a></li>'.format(**locals()))
+		index_links.append('<li><a href=pages/{slug}.html>{title}</a></li>'.format(**locals()))
+		links.append('<li><a href=./pages/{slug}.html>{title}</a></li>'.format(**locals()))
+index_links.append('</ul>')
 links.append('</ul>')
+
 
 for post_path in post_paths:
 	md = Markdown(extensions=['markdown.extensions.meta'])
@@ -24,4 +28,4 @@ for post_path in post_paths:
 			p.write('\n'.join([header.read()] + links + [html_title] + [html] + [footer.read()]))
 
 with open('static/header.html') as header, open('static/footer.html') as footer, open('index.html','w') as index:
-	index.write('\n'.join([header.read()] + links + [footer.read()]))
+	index.write('\n'.join([header.read()] + index_links + [footer.read()]))
